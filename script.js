9398e198684d4fa46290d0c842c34d4eec5e3311
sumaya-jahan <sumayajahan765@gmail.com>
2026-03-25 22:52:37 +0600

    js added for showing title only

--- script.js
+++ script.js
@@ -5,5 +5,16 @@ fetch('https://phi-lab-server.vercel.app/api/v1/lab/issues')
     .then(data =>{
         allissues=data.data;
         console.log(allissues)
+        displayIssues(allissues)
     });
 
+function displayIssues(issues) {
+    const container=document.getElementById('issue-container');
+    container.innerHTML="";
+
+    issues.forEach(issue => {
+        const div=document.createElement('div');
+        div.innerText=issue.title;
+        container.appendChild(div);
+    });
+}
