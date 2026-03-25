8ab35bf554c283a7502edb516ce5105e5383b4f4
sumaya-jahan <sumayajahan765@gmail.com>
2026-03-25 20:36:43 +0600

    js added for sign in page

--- sign-in.js
+++ sign-in.js
@@ -1,3 +1,21 @@
 
+document.getElementById('signIn-btn').addEventListener('click', function () {
 
+    const userInput = document.getElementById('input-user');
+    const userName = userInput.value
+    console.log(userName)
+
+    const passInput = document.getElementById('input-password');
+    const password = passInput.value
+    console.log(password)
+
+    if (userName == 'admin' && password == 'admin123') {
+        alert('Sign In Successful')
+        window.location.assign("home.html")
+    } else {
+        alert('Sign In Failed')
+        return
+    }
+
+});
 
